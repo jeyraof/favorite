c27d7f32a2df5bd7a6fa09a74e26445f8b6d86fc
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-04-20 04:03:18 +0900

    새 오브젝트 추가되게

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
   // toggle new-item form
-  var $newForm = $('.new-item');
+  var $newDiv = $('.new-item');
   $('.btn-new').click(function () {
-    $newForm.toggleClass('on');
+    $newDiv.toggleClass('on');
   });
 
-
+  var $newForm = $newDiv.find('form');
+  $('.btn-new-submit').click(function() {
+    $.post(
+      $newForm.data('validate'),
+      $newForm.serialize(),
+      function(data) {
+        if (data.ok) {
+          $newForm.submit();
+        } else {
+          alert(data.msg || '에러가 발생했습니다. 관리자에게 문의해 주세요.');
+        }
+      }
+    )
+  });
 });
