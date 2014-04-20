ae98be1163ada7561dae08487c6f60bd0ec3f22a
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-04-20 10:14:14 +0900

    업로드 설정과 확장자

--- config.sample.py
+++ config.sample.py
@@ -4,4 +4,8 @@
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///favorite.db'
+
     ITEM_EA = 10
+
+    UPLOAD_DIR = 'data/'
+    ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
